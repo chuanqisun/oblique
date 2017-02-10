3c2b06aea18221d5619ced412081d7c7f7a90337
Chuanqi <henrysun918@gmail.com>
2017-02-09 20:39:53 -0800

    local storage and random draw

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,49 @@
+/* contracts */
+interface StrategyList {
+    version: string;
+    strategies: string[];
+}
+
+interface AppState {
+    version: string;
+    viewedStrategies: string[];
+    unviewedStrategies: string[];
+    currentStrategy: string;
+}
+
+/* constants */
+const appStateKey = 'appState';
+const strategyListPath = './strategy-list.json';
+
 /* model */
-class Model {
-    private strategies: string[];
-    private currentStrategy: number;
+class Model implements AppState {
+    public version: string;
+    public viewedStrategies: string[];
+    public unviewedStrategies: string[];
+    public currentStrategy: string;
 
-    constructor() {
-        this.currentStrategy = 0;
-        this.strategies = [];
+    public init(state: AppState) {
+        this.version = state.version;
+        this.viewedStrategies = state.viewedStrategies;
+        this.unviewedStrategies = state.unviewedStrategies;
+        this.currentStrategy = state.currentStrategy;
+
     }
 
-    public setStrategies(stratigies: string[]): void {
-        this.strategies = stratigies;
+    public drawNextStrategy(): void {
+        if (this.unviewedStrategies.length === 0) {
+            this.resetViewingHistory();
+        }
+        const unviewedStrategies = this.unviewedStrategies;
+        const removeAt = Math.floor(Math.random()*unviewedStrategies.length);
+        const chosenStrategy = unviewedStrategies.splice(removeAt, 1)[0];
+        this.currentStrategy = chosenStrategy;
+        this.viewedStrategies.push(chosenStrategy);
     }
 
-    public getCurrentStrategy(): string {
-        return this.strategies[this.currentStrategy];
-    }
-
-    public getNextStrategy(): string {
-        this.currentStrategy = (this.currentStrategy + 1) % this.strategies.length;
-        return this.getCurrentStrategy();
+    private resetViewingHistory() {
+        this.unviewedStrategies = this.viewedStrategies;
+        this.viewedStrategies = [];
     }
 }
 
@@ -30,26 +55,56 @@ class ViewModel {
     // view -> view-model
     private nextButton: HTMLElement;
 
-    constructor(private model: Model) {
+    constructor(private model: Model, private service: Service) {
         this.initModel().then(() => {
             this.initViewModel();
         });
     }
 
-    private initViewModel() {
+    private initViewModel(): void {
         this.strategy = document.getElementById('o-strategy');
-        this.strategy.innerHTML = this.model.getCurrentStrategy();
+        this.strategy.innerHTML = this.model.currentStrategy;
         this.nextButton = document.getElementById('o-next');
         this.nextButton.addEventListener("click", () => {
-            this.strategy.innerHTML = this.model.getNextStrategy();
-            console.dir('click');
+            this.model.drawNextStrategy();
+            this.service.cacheAppState(this.model);
+            this.strategy.innerHTML = this.model.currentStrategy;
         });
     }
 
     private async initModel() {
-        const strategies = await Util.loadJson<string[]>('./strategies.json');
-        this.model.setStrategies(strategies);
+        const appState = await this.service.getAppState();
+        this.model.init(appState);
     }
+}
+
+/* service */
+class Service {
+    public async getAppState() {
+        return new Promise<AppState>((resolve, reject) => {
+            const cachedAppStateString = localStorage.getItem(appStateKey);
+            const cachedAppState: AppState = cachedAppStateString ? JSON.parse(cachedAppStateString) : null;
+
+            Util.loadJson<StrategyList>(strategyListPath).then(strategyList => {
+                if (!cachedAppState || strategyList.version !== cachedAppState.version) {
+                    const appState: AppState = {
+                        version: strategyList.version,
+                        viewedStrategies: [],
+                        unviewedStrategies: strategyList.strategies,
+                        currentStrategy: null,
+                    }
+                    resolve(appState);
+                    this.cacheAppState(appState);
+                } else {
+                    resolve(cachedAppState);
+                }
+            });
+        });
+    }
+
+    public cacheAppState(appState: AppState): void {
+        localStorage.setItem(appStateKey, JSON.stringify(appState));
+    } 
 }
 
 /* helpers */
@@ -75,5 +130,5 @@ class Util {
 
 /* init */
 const model = new Model();
-const viewModel = new ViewModel(model);
-
+const service = new Service();
+const viewModel = new ViewModel(model, service);
